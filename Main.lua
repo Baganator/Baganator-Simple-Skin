04e4e86a9dd2af2bc69563861738c46dcd8c8c3b
plusmouse <plusmouse@protonmail.com>
2024-06-12 22:39:28 +0100

    Improve edges (again)

--- Main.lua
+++ Main.lua
@@ -15,7 +15,7 @@ local backdropInfo = {
   tileEdge = true,
   tileSize = 32,
   edgeSize = 6,
-  insets = {left = 1, right = 1, top = 1, bottom = 1},
+  insets = {left = 0, right = 0, top = 0, bottom = 0},
 }
 
 local color = CreateColor(0.05, 0.05, 0.05)
